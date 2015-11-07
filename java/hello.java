4fa4c11221d7d3f09c4d4b3635d71b9e4a1d2302
Tyler Carr <tylrcarr@gmail.com>
2015-11-07 16:01:38 -0500

    save

---
+++ java/hello.java
@@ -0,0 +1,33 @@
+import skilstak.c;
+
+
+public class hello{
+
+    public static void color(String message) {
+        while(true){
+            System.out.println(c.rc() + message + " ");
+        }
+
+    }
+    public static void plain(String message) {
+        while(true){
+            System.out.println(message + " ");
+        }
+    }
+    public static void multi(String message) throws InterruptedException {
+        while(true){
+
+
+        }   
+
+    }
+}
+
+
+
+
+
+
+
+
+}
