5635394a2c15f32d7fe32116c3bd035bb6877adf
Tyler Carr <tylrcarr@gmail.com>
2015-11-14 17:12:39 -0500

    save

--- go/src/hello/main.go
+++ go/src/hello/main.go
@@ -1,9 +1,50 @@
 package main
 
-import {
+import (
 	"fmt"
 	c "github.com/skilstak/go/colors"
 	"os"
-	"strings"
 	"time"
+)
+
+func Plain(message string) {
+	fmt.Println("Hello World!")
+}
+
+func Loop(message string) {
+	for {
+		fmt.Println("Hello World!")
+	}
+}
+
+func Color(message string) {
+	for {
+		fmt.Println(c.Rc() + message)
+	}
+}
+
+func Multi(message string) {
+	for {
+		fmt.Println(c.Clear + c.Multi(message))
+		time.Sleep(2 * time.Millisecond)
+	}
+
+}
+
+func main() {
+	var name = "cis scum"
+	var option = os.Args[1]
+	if option.hasPrefix(os.Args[1], "-") {
+		var name = Args[2]
+		if option == "-m" {
+			Multi(name)
+		} else if option[2] == "-c" {
+			Color(name)
+		} else {
+			fmt.Println("Use -c, -m, or no - at all.")
+		}
+	} else {
+		var name = Args[1]
+		Plain(name)
+	}
 }
